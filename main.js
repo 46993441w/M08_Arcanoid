31f6f33882455b3c39515a70d6eac3a65264ab70
David Juez <46993441w@iespoblenou.org>
2016-04-04 16:23:40 +0200

    afegir vides, perdre vides, fer que la pilota reboti diferent segons on toca de la barra

--- main.js
+++ main.js
@@ -10,9 +10,10 @@ var mainState = (function (_super) {
     function mainState() {
         _super.apply(this, arguments);
         this.contador = 0;
-        this.primerClick = true;
+        this.bolaEnBarra = true;
         this.MAX_SPEED = 250; // pixels/second
         this.ACCELERATION = 750; // pixels/second/second
+        this.VIDES = 3;
     }
     mainState.prototype.preload = function () {
         _super.prototype.preload.call(this);
@@ -26,29 +27,32 @@ var mainState = (function (_super) {
     };
     mainState.prototype.create = function () {
         _super.prototype.create.call(this);
-        // fer que no colisioni per la par de sota del mon
+        // fer que no colisioni per la paret de sota del mon
         this.game.physics.arcade.checkCollision.down = false;
         this.crearJugador();
         this.createBarres();
         this.cursor = this.input.keyboard.createCursorKeys();
-        this.contadorText = this.game.add.text(400, 35, "Contador  0 ", {
+        this.contadorText = this.game.add.text(400, 35, "Contador:  0 ", {
             font: "25px Arial",
             fill: "#ff0044",
             align: "center"
         });
-        this.introText = this.game.add.text(this.game.world.centerX, 400, '- click to start -', { font: "40px Arial", fill: "#ff0044", align: "center" });
+        this.introText = this.game.add.text(this.game.world.centerX, 400, '- Click per a Començar -', { font: "40px Arial", fill: "#ff0044", align: "center" });
         this.introText.anchor.setTo(0.5, 0.5);
     };
     mainState.prototype.crearJugador = function () {
-        this.barra = this.add.sprite(this.world.centerX, this.world.height - 50, 'barra');
         this.bola = this.add.sprite(this.world.centerX, this.world.height - 100, 'bola');
-        this.barra.anchor.setTo(0.5, 0.5);
         this.bola.anchor.setTo(0.5, 0.5);
+        this.bola.checkWorldBounds = true;
+        this.bola.health = this.VIDES;
+        this.barra = this.add.sprite(this.world.centerX, this.world.height - 50, 'barra');
+        this.barra.anchor.setTo(0.5, 0.5);
         this.physics.enable(this.barra, Phaser.Physics.ARCADE); // activar la fisica de la barra
         this.physics.enable(this.bola, Phaser.Physics.ARCADE); // activar la fisica de la bola
         this.bola.body.maxVelocity.setTo(this.MAX_SPEED, this.MAX_SPEED); // x, y
         this.bola.body.collideWorldBounds = true;
         this.bola.body.bounce.setTo(1.2);
+        this.bola.events.onOutOfBounds.add(this.perdreVida, this);
         this.barra.body.bounce.setTo(1.2);
         this.barra.body.immovable = true;
         this.barra.body.collideWorldBounds = true;
@@ -65,28 +69,73 @@ var mainState = (function (_super) {
         for (var i = 0; i < 4; i++) {
             var y = (i * 32 + 32) + 50;
             for (var j = 0; j < 8; j++) {
-                var x = j * 63 + 63;
+                var x = j * 63 + 75;
                 var rectangle = new Rectangle(this.game, x, y, colors[i]);
                 this.rectangles.add(rectangle);
             }
         }
     };
+    mainState.prototype.perdreVida = function () {
+        this.bola.damage(1);
+        if (this.bola.health == 0) {
+        }
+        else {
+            this.bolaEnBarra = true;
+            this.bola.reset(this.barra.body.x, this.barra.y - 26);
+            this.bola.animations.stop();
+        }
+    };
     mainState.prototype.update = function () {
         _super.prototype.update.call(this);
         this.moureBarra();
-        if (this.input.activePointer.isDown && this.primerClick) {
-            this.primerClick = false;
+        if (this.input.activePointer.isDown && this.bolaEnBarra) {
             this.bola.body.velocity.x = 200;
             this.bola.body.velocity.y = 190;
             this.introText.setText("");
+            this.bolaEnBarra = false;
         }
-        this.physics.arcade.collide(this.bola, this.barra);
-        this.physics.arcade.collide(this.bola, this.rectangles, this.trencaRectangle, null, this);
+        if (this.bolaEnBarra) {
+            this.bola.body.x = this.barra.x;
+        }
+        else {
+            this.physics.arcade.collide(this.bola, this.barra, this.bolaRebot, null, this);
+            this.physics.arcade.collide(this.bola, this.rectangles, this.trencaRectangle, null, this);
+        }
+    };
+    mainState.prototype.bolaRebot = function (bola, barra) {
+        var diff = 0;
+        if (bola.x < barra.x) {
+            // Bola esta a la part esquerra de la barra
+            diff = barra.x - bola.x;
+            bola.body.velocity.x = (-10 * diff);
+        }
+        else if (bola.x > barra.x) {
+            // Bola esta a la part dreta de la barra
+            diff = bola.x - barra.x;
+            bola.body.velocity.x = (10 * diff);
+        }
+        else {
+            // Bola esta al centre de la barra
+            //  Add a little random X to stop it bouncing straight up!
+            bola.body.velocity.x = 2 + Math.random() * 8;
+        }
     };
     mainState.prototype.trencaRectangle = function (bola, rectangle) {
         rectangle.kill();
         this.contador++;
-        this.contadorText.setText("Contador  " + this.contador);
+        this.contadorText.setText("Contador: " + this.contador);
+        if (this.rectangles.countLiving() == 0) {
+            //  New level starts
+            this.introText.setText('- Seguent Nivell -');
+            //  Let's move the ball back to the paddle
+            this.bolaEnBarra = true;
+            this.bola.body.velocity.set(0);
+            this.bola.x = this.barra.x;
+            this.bola.y = this.barra.y - 26;
+            this.bola.animations.stop();
+            //  And bring the bricks back from the dead :)
+            this.rectangles.callAll('revive');
+        }
     };
     mainState.prototype.moureBarra = function () {
         if (this.cursor.left.isDown) {
@@ -101,6 +150,13 @@ var mainState = (function (_super) {
         // fer que funcioni amb el ratolí
         if (this.input.activePointer.active) {
             this.barra.x = this.input.x;
+        }
+        // evitar que surti del mon i aixi no faci el rebot extrany
+        if (this.barra.x < 54) {
+            this.barra.x = 54;
+        }
+        else if (this.barra.x > this.game.width - 54) {
+            this.barra.x = this.game.width - 54;
         }
     };
     return mainState;
--- main.ts
+++ main.ts
@@ -11,10 +11,11 @@ class mainState extends Phaser.State {
     private contadorText:Phaser.Text;
     private introText:Phaser.Text;
     private contador = 0;
-    private primerClick = true;
+    private bolaEnBarra = true;
 
     private MAX_SPEED = 250; // pixels/second
     private ACCELERATION = 750; // pixels/second/second
+    private VIDES = 3;
 
     preload():void {
         super.preload();
@@ -32,7 +33,7 @@ class mainState extends Phaser.State {
     create():void {
         super.create();
 
-        // fer que no colisioni per la par de sota del mon
+        // fer que no colisioni per la paret de sota del mon
         this.game.physics.arcade.checkCollision.down = false;
 
         this.crearJugador();
@@ -40,20 +41,24 @@ class mainState extends Phaser.State {
 
         this.cursor = this.input.keyboard.createCursorKeys();
 
-        this.contadorText = this.game.add.text(400, 35, "Contador  0 ", {
+        this.contadorText = this.game.add.text(400, 35, "Contador:  0 ", {
             font: "25px Arial",
             fill: "#ff0044",
             align: "center"
         });
-        this.introText = this.game.add.text(this.game.world.centerX, 400, '- click to start -', { font: "40px Arial", fill: "#ff0044", align: "center" });
+        this.introText = this.game.add.text(this.game.world.centerX, 400, '- Click per a Començar -', { font: "40px Arial", fill: "#ff0044", align: "center" });
         this.introText.anchor.setTo(0.5, 0.5);
     }
 
     private crearJugador(){
-        this.barra = this.add.sprite(this.world.centerX, this.world.height - 50, 'barra');
         this.bola = this.add.sprite(this.world.centerX, this.world.height - 100, 'bola');
-        this.barra.anchor.setTo(0.5, 0.5);
         this.bola.anchor.setTo(0.5, 0.5);
+        this.bola.checkWorldBounds = true;
+        this.bola.health = this.VIDES;
+
+        this.barra = this.add.sprite(this.world.centerX, this.world.height - 50, 'barra');
+        this.barra.anchor.setTo(0.5, 0.5);
+
 
         this.physics.enable(this.barra, Phaser.Physics.ARCADE); // activar la fisica de la barra
         this.physics.enable(this.bola, Phaser.Physics.ARCADE); // activar la fisica de la bola
@@ -61,8 +66,9 @@ class mainState extends Phaser.State {
         this.bola.body.maxVelocity.setTo(this.MAX_SPEED, this.MAX_SPEED); // x, y
         this.bola.body.collideWorldBounds = true;
         this.bola.body.bounce.setTo(1.2);
-        this.barra.body.bounce.setTo(1.2);
+        this.bola.events.onOutOfBounds.add(this.perdreVida, this);
 
+        this.barra.body.bounce.setTo(1.2);
         this.barra.body.immovable = true;
         this.barra.body.collideWorldBounds = true;
     }
@@ -82,12 +88,24 @@ class mainState extends Phaser.State {
         for(var i = 0; i < 4; i++){
             var y = (i*32+32)+50;
             for(var j = 0; j < 8; j++){
-                var x = j*63+63;
+                var x = j*63+75;
                 var rectangle = new Rectangle (this.game, x, y, colors[i]);
                 this.rectangles.add(rectangle);
             }
 
         }
+    }
+
+    private perdreVida():void {
+        this.bola.damage(1);
+        if (this.bola.health == 0) {
+            //gameOver();
+        } else {
+            this.bolaEnBarra = true;
+            this.bola.reset(this.barra.body.x, this.barra.y - 26);
+            this.bola.animations.stop();
+        }
+
     }
 
     update():void {
@@ -96,22 +114,59 @@ class mainState extends Phaser.State {
         this.moureBarra();
 
 
-        if(this.input.activePointer.isDown && this.primerClick){
-            this.primerClick = false;
+        if(this.input.activePointer.isDown && this.bolaEnBarra){
             this.bola.body.velocity.x= 200;
             this.bola.body.velocity.y= 190;
             this.introText.setText("");
+            this.bolaEnBarra = false;
         }
 
-        this.physics.arcade.collide(this.bola, this.barra);
-        this.physics.arcade.collide(this.bola, this.rectangles, this.trencaRectangle, null, this);
+        if (this.bolaEnBarra) {
+            this.bola.body.x = this.barra.x;
+        } else {
+            this.physics.arcade.collide(this.bola, this.barra, this.bolaRebot, null, this);
+            this.physics.arcade.collide(this.bola, this.rectangles, this.trencaRectangle, null, this);
+        }
     }
 
-    private trencaRectangle(bola:Phaser.Sprite,rectangle:Rectangle){
+    private bolaRebot(bola:Phaser.Sprite, barra:Phaser.Sprite) {
+
+        var diff = 0;
+
+        if (bola.x < barra.x) {
+            // Bola esta a la part esquerra de la barra
+            diff = barra.x - bola.x;
+            bola.body.velocity.x = (-10 * diff);
+        } else if (bola.x > barra.x) {
+            // Bola esta a la part dreta de la barra
+            diff = bola.x - barra.x;
+            bola.body.velocity.x = (10 * diff);
+        }
+        else {
+            // Bola esta al centre de la barra
+            //  Add a little random X to stop it bouncing straight up!
+            bola.body.velocity.x = 2 + Math.random() * 8;
+        }
+    }
+
+    private trencaRectangle(bola:Phaser.Sprite, rectangle:Rectangle){
         rectangle.kill();
         this.contador++;
-        this.contadorText.setText("Contador  " + this.contador);
+        this.contadorText.setText("Contador: " + this.contador);
+        if (this.rectangles.countLiving() == 0) {
+            //  New level starts
+            this.introText.setText('- Seguent Nivell -');
 
+            //  Let's move the ball back to the paddle
+            this.bolaEnBarra = true;
+            this.bola.body.velocity.set(0);
+            this.bola.x = this.barra.x;
+            this.bola.y = this.barra.y - 26;
+            this.bola.animations.stop();
+
+            //  And bring the bricks back from the dead :)
+            this.rectangles.callAll('revive');
+        }
     }
 
     private moureBarra(){
@@ -126,6 +181,13 @@ class mainState extends Phaser.State {
         // fer que funcioni amb el ratolí
         if (this.input.activePointer.active) {
             this.barra.x = this.input.x;
+        }
+
+        // evitar que surti del mon i aixi no faci el rebot extrany
+        if (this.barra.x < 54) {
+            this.barra.x = 54;
+        } else if (this.barra.x > this.game.width - 54) {
+            this.barra.x = this.game.width - 54;
         }
     }
 }
